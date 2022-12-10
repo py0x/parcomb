4c92912716f81c69bd235b2b342de3f9243612ac
PY <py@pypy.info>
2022-12-10 16:37:18 +0800

    update json parser

--- examples/json.rs
+++ examples/json.rs
@@ -40,16 +40,20 @@ fn number_p<'a>(input: &'a str) -> JsonResult<'a> {
     par.parse(input)
 }
 
-fn string_p<'a>(input: &'a str) -> JsonResult<'a> {
+fn raw_string_p<'a>(input: &'a str) -> ParseResult<&'a str, String, String> {
     let str_char_ptn = r#"[^\\"]"#;
     let esc_char_ptn = r#"\\[\\/"bfnrt]"#;
     let utf16_char_ptn = r#"\\u[a-fA-F0-9]{4}"#; // todo: should conver to u16
 
     let json_str_ptn = format!(r#""(({str_char_ptn})|({esc_char_ptn})|({utf16_char_ptn}))*""#);
 
-    let par = reg(&json_str_ptn).map(|s| JsonValue::String(s));
+    let par = reg(&json_str_ptn);
 
     par.parse(input)
+}
+
+fn string_p<'a>(input: &'a str) -> JsonResult<'a> {
+    raw_string_p.map(|s| JsonValue::String(s)).parse(input)
 }
 
 fn array_p<'a>(input: &'a str) -> JsonResult<'a> {
@@ -62,7 +66,19 @@ fn array_p<'a>(input: &'a str) -> JsonResult<'a> {
 }
 
 fn object_p<'a>(input: &'a str) -> JsonResult<'a> {
-    todo!()
+    let kv_member = raw_string_p.and_l(lit_sp(":")).and(value_p);
+    let kv_members = lst_sep_empt(kv_member, lit_sp(","));
+
+    let obj = lit_sp("{").and_r(kv_members).and_l(lit_sp("}")).map(|kvs| {
+        let mut hmap = HashMap::<String, JsonValue>::new();
+        for (k, v) in kvs.into_iter() {
+            hmap.insert(k, v);
+        }
+
+        JsonValue::Object(hmap)
+    });
+
+    obj.parse(input)
 }
 
 fn value_p<'a>(input: &'a str) -> JsonResult<'a> {
@@ -76,8 +92,33 @@ fn value_p<'a>(input: &'a str) -> JsonResult<'a> {
     par.parse(input)
 }
 
-fn main() {
-    let input = r#" [  false,99, "\u2192the", true, true, 32, 1.321, "aaa", null ] "#;
+fn json_p<'a>(input: &'a str) -> JsonResult<'a> {
+    let par = spaces().and_r(value_p).and_l(spaces());
+    let (o, i) = par.parse(input)?;
 
-    println!("{:#?}", value_p.parse(input));
+    if i.len() > 0 {
+        return Err(format!("invalid json"));
+    }
+
+    return Ok((o, i));
+}
+
+fn main() {
+    let input = r#"
+        {
+            "Number Array": [0, 12.3, -1E+2],
+            "String": "hello\n\tworld",
+            "Bool": [true, false],
+            "Null": null,
+            "URL": "http://www.json.org/id/1234",
+            "Array": [false, 99, "\u2192the", true, 32, 1.321, "aaa", null ],
+            "Nested Obj": {
+                "o1": null,
+                "o2": {},
+                "Empty Array": []
+            }
+        }
+    "#;
+
+    println!("{:#?}", json_p.parse(input));
 }
